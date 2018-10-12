beb2a4f45a5f7f3f0b59da9d88cf70aaecf39f70
Jalen Jackson <jaydiddy72@csu.fullerton.edu>
2018-10-12 10:40:55 -0700

    Updated Car w/ ProBuilds Models
    Error: Car isn't fully balanced, leans off to the side after it falls

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, moveVertical);
+        body.AddForce(move * speed);
+	}
+}
