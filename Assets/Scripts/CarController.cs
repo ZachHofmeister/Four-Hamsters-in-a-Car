2e711523e37ba653dac87c9dbbbca6e7626d8e04
ZachHofmeister <zachhof@me.com>
2018-10-19 12:21:33 -0700

    Basic ability for hamster to control car.
    This overwrites some of the work to do with camera follow. Talk to me and we can try to resolve it, but for now this needs to be committed. In the future, don't wait to push your work, because it wasn't there when I wrote all of this.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< HEAD
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+	public bool steering;
+}
+
+public class CarController : MonoBehaviour {
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+	public float maxBrakeTorque;
+    public float maxSteeringTorque;
+	public float gasPercent, brakePercent;
+	public ControlPoint cpGas, cpBrake, cpSteering;
+
+	public void ApplyLocalPositionToVisuals(WheelCollider collider) {
+        if (collider.transform.childCount == 0)
+            return;
+=======
 public class CarController : MonoBehaviour {
 
     public float motorForce, steerForce, brakeForce;
     public WheelCollider leftBackWheel, leftFrontWheel, rightBackWheel, rightFrontWheel;
 
     private float v, h;
+>>>>>>> 73627d04f29ce23c1b03f95db840882ec3c81c9f
 
     private void Update()
     {
@@ -30,6 +53,35 @@ public class CarController : MonoBehaviour {
         rightFrontWheel.transform.GetChild(0).rotation = rightFrontRotation;
     }
 
+<<<<<<< HEAD
+    public void FixedUpdate() {
+		brakePercent += (cpBrake.hamster != null && Input.GetKey (KeyCode.S) )? 0.1f : -0.1f;
+		brakePercent = Mathf.Clamp (brakePercent, 0f, 1f);
+		gasPercent += (cpGas.hamster != null && Input.GetKey (KeyCode.W))? 0.1f : -0.1f;
+		gasPercent -= brakePercent; //Allows brakes to override gas, otherwise car would continue forwards if brake is held after and while gas is held.
+		gasPercent = Mathf.Clamp (gasPercent, 0f, 1f);
+
+		float motor = maxMotorTorque * gasPercent; //Seperate gas pedal
+		float brake = maxBrakeTorque * brakePercent; //Seperate brake pedal
+
+		float steering = maxSteeringTorque * (cpSteering.hamster != null? Input.GetAxis("Horizontal") : 0); //Seperate steering
+
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.leftWheel.brakeTorque = brake;
+				axleInfo.rightWheel.motorTorque = motor;
+				axleInfo.rightWheel.brakeTorque = brake;
+			}
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+=======
     private void FixedUpdate()
     {
         float v = Input.GetAxis("Vertical") * motorForce;
@@ -61,6 +113,7 @@ public class CarController : MonoBehaviour {
         {
             leftBackWheel.brakeTorque = 0;
             rightBackWheel.brakeTorque = 0;
+>>>>>>> 73627d04f29ce23c1b03f95db840882ec3c81c9f
         }
     }
 
--- Assets/Scripts/HamsterController.cs
+++ Assets/Scripts/HamsterController.cs
@@ -6,7 +6,8 @@ public class HamsterController : MonoBehaviour {
 
 	public Rigidbody rb;
 	public float speed = 4.0f, jumpSpeed = 3.0f, gravity = 1.0f;
-	public bool jumping;
+	public bool jumping, grounded, beenControlling;
+	public ControlPoint cpCurrent;
 
 	// Use this for initialization
 	void Start () {
@@ -15,24 +16,53 @@ public class HamsterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (cpCurrent != null && Input.GetKeyDown (KeyCode.F) && beenControlling) {
+			cpCurrent.hamster = null;
+			cpCurrent = null;
+			beenControlling = false;
+		}
 	}
 
 	void FixedUpdate() {
-		Vector3 vertical, horizontal, moveForce;
-		vertical = transform.forward * Input.GetAxisRaw("Vertical"); //Movement force for forward and back
-		horizontal = transform.right * Input.GetAxisRaw("Horizontal"); //Movement force for left/right
-		moveForce = vertical + horizontal; //Combined movement forces
-		Vector3 velocityChange = moveForce.normalized * speed - rb.velocity; //The difference in velocity between past and future.
-		rb.AddForce(velocityChange, ForceMode.VelocityChange); //Applies movement
+		if (cpCurrent == null) {
+			Vector3 vertical, horizontal, moveForce;
+			vertical = transform.forward * Input.GetAxisRaw ("Vertical"); //Movement force for forward and back
+			horizontal = transform.right * Input.GetAxisRaw ("Horizontal"); //Movement force for left/right
+			moveForce = vertical + horizontal; //Combined movement forces
+			Vector3 velocityChange = moveForce.normalized * speed - new Vector3(rb.velocity.x, 0, rb.velocity.z); //The difference in velocity between past and future.
+			rb.AddForce (velocityChange, ForceMode.VelocityChange); //Applies movement
 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			rb.velocity += transform.up * jumpSpeed / gravity;
-			jumping = true;
+			if ((Input.GetKeyDown (KeyCode.Space) || Input.GetKey(KeyCode.Space)) && !jumping && grounded) {
+				rb.velocity += transform.up * jumpSpeed / gravity;
+				jumping = true;
+			}
+
+			if (!grounded) {
+				rb.AddForce (transform.up * -gravity * 10 * rb.mass); //Applies gravity
+			}
+
+			transform.GetChild (0).LookAt (transform.position + moveForce); //Animates the face looking in the direction of movement
+
+			grounded = false;
 		}
+		if (cpCurrent != null && !beenControlling) {
+			beenControlling = true;
+		}
+	}
 
-		rb.AddForce(transform.up * -gravity * 10 * rb.mass); //Applies gravity
+	void OnCollisionStay(Collision col) {
+		grounded = true;
+		jumping = false;
+	}
 
-		transform.GetChild(0).LookAt(transform.position + moveForce); //Animates the face looking in the direction of movement
+	void OnTriggerStay (Collider col) {
+		Debug.Log (col.name);
+		if (col.tag == "ControlPoint" && col.GetComponent<ControlPoint> ().hamster == null && cpCurrent == null && (Input.GetKeyDown (KeyCode.E) || Input.GetKey(KeyCode.E))) {
+			Debug.Log ("STUFF");
+			ControlPoint cp = col.GetComponent<ControlPoint> ();
+			cp.hamster = this;
+			Debug.Log (cp.hamster.name);
+			cpCurrent = cp;
+		}
 	}
 }
