4d44fd81c7f4bf85e8c33fcd05cbcdc308e83736
Jalen Jackson <jaydiddy72@csu.fullerton.edu>
2018-10-26 23:36:15 -0700

    Added NavComponents to implement AI script to a car

---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarAI : MonoBehaviour {
+
+    public Transform target;
+
+    private NavMeshAgent navMesh;
+
+	// Use this for initialization
+	void Start () {
+        navMesh = GetComponent<NavMeshAgent>();
+	}
+
+    private void FixedUpdate()
+    {
+        navMesh.SetDestination(target.position);
+
+    }
+}
