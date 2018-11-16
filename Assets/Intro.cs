fa102b98aab41e8daf6f0485fa4a698d24728738
lorena <lorenamacias@csu.fullerton.edu>
2018-11-16 13:08:26 -0800

    menu/intro

---
+++ Assets/Intro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+    public canvas[] images; 
+
+	void Start () {
+        for (int i = 0; i < 4; i++)
+        {
+
+            System.Threading.Thread.Sleep(2000); //pause for 2 seconds
+        }
+	    
+	}
+	
+	void Update () { 
+        
+        //make a loop to display each image with a delay
+
+        if (Input.anyKey) //if the user presses any key, skip to main menu
+        {
+            Application.LoadLevel("MainMenu");
+        }
+	}
+}
